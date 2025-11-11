4243d68bb0e2d5bc2f5cb93a8e4f4bb4e8e3e8bf
Diego Infantas Peña <127537754+DiInfantas@users.noreply.github.com>
2025-11-11 19:05:51 -0300

    Creacion de modelo y signals usuarios

--- Back/usuarios/models.py
+++ Back/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PerfilUsuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=200, blank=True, null=True)
+    comuna = models.CharField(max_length=100, blank=True, null=True)
+    ciudad = models.CharField(max_length=100, blank=True, null=True)
+    codigo_postal = models.CharField(max_length=20, blank=True, null=True)
+    es_vendedor = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Perfil de {self.usuario.username}'
